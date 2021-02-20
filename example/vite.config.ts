0e3063e32c6a0b97b1b0c91df25993b159fdae38
gxmari007 <392041299@qq.com>
2021-02-20 10:01:44 +0800

    chore: delete publishConfig

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import eslint from '@gxmari007/vite-plugin-eslint';
+import eslint from 'vite-plugin-eslint';
 
 export default defineConfig({
   plugins: [vue(), eslint({ include: ['./*.ts', './*.vue'] })],
