fd8803bcf43fa47989927b36dee9a1445ebc15a5
gxmari007 <392041299@qq.com>
2021-03-06 10:29:32 +0800

    chore: config file replace to json file

--- example/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  parser: 'vue-eslint-parser',
-  extends: [
-    'plugin:vue/vue3-essential',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-    'plugin:prettier/recommended',
-  ],
-};
