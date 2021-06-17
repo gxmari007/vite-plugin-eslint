67bbfaf982cf19886f1b9b1f92c248eda4596afa
James Gao <392041299@qq.com>
2021-06-17 16:56:17 +0800

    chore: add react example

--- example/react/vite.config.ts
+++ example/react/vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import reactRefresh from '@vitejs/plugin-react-refresh';
 import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), eslint()],
+  plugins: [reactRefresh(), eslint()],
 });
