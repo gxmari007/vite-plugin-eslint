35074388170413d9911d9636658eebb83977f9d7
gxmari007 <392041299@qq.com>
2022-04-16 18:24:15 +0800

    chore: edit prettier config

--- src/utils.ts
+++ src/utils.ts
@@ -1,32 +1,32 @@
-import * as qs from 'querystring';
-import * as path from 'path';
-import type { ESLint } from 'eslint';
+import * as qs from 'querystring'
+import * as path from 'path'
+import type { ESLint } from 'eslint'
 
 export interface Options {
   /** The cache is enabled by default to decrease execution time */
-  cache?: boolean;
+  cache?: boolean
   /** auto fix source code */
-  fix?: boolean;
+  fix?: boolean
   /** A single file, or array of files, to include when linting */
-  include?: string | string[];
+  include?: string | string[]
   /** A single file, or array of files, to exclude when linting */
-  exclude?: string | string[];
+  exclude?: string | string[]
   /** Custom error formatter or the name of a built-in formatter */
-  formatter?: string | ESLint.Formatter;
+  formatter?: string | ESLint.Formatter
   /** The warings found will be emitted */
-  throwOnWarning?: boolean;
+  throwOnWarning?: boolean
   /** The errors found will be emitted */
-  throwOnError?: boolean;
+  throwOnError?: boolean
 }
 
 export function normalizePath(id: string): string {
-  return path.relative(process.cwd(), id).split(path.sep).join('/');
+  return path.relative(process.cwd(), id).split(path.sep).join('/')
 }
 
 export function checkVueFile(id: string): boolean {
-  if (!id.includes('?')) return false;
+  if (!id.includes('?')) return false
 
-  const rawQuery = id.split('?', 2)[1];
+  const rawQuery = id.split('?', 2)[1]
 
-  return qs.parse(rawQuery).vue !== null ? true : false;
+  return qs.parse(rawQuery).vue !== null ? true : false
 }
