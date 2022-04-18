3f55d52daf86db65f17418dec5cbe8fc86b55305
James Gao <392041299@qq.com>
2022-04-18 19:37:27 +0800

    refactor: (wip) workspace

--- playground/vue/vite.config.ts
+++ playground/vue/vite.config.ts
@@ -5,5 +5,5 @@ import eslint from '../../'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), eslint({ include: 'playground/vue/src/**/*', cacheLocation: '.eslintcache' })],
+  plugins: [vue(), eslint()],
 })
