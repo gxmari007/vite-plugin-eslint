60740a9c5eec6bb8964fa0566bfb8bf136081923
gxmari007 <392041299@qq.com>
2022-08-16 21:02:26 +0800

    chore: update deps

--- playground/react-ts/src/App.tsx
+++
@@ -1,3 +0,0 @@
-export default function App() {
-  return <div>react-ts</div>
-}
--- playground/react-ts/vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import eslint from 'vite-plugin-eslint'
-
-export default defineConfig({
-  plugins: [react(), eslint()],
-})
